a7e867ebb96edc407c6a03e1307a7fe25daf626a
YJ <yangjing.yj56@gmail.com>
2021-03-09 12:20:17 +0800

    updated to combine reducers

--- src/hooks/useSlice.ts
+++ src/hooks/useSlice.ts
@@ -1,5 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../store';
+import { RootState } from '../store/rootReducer';
+import { AppDispatch } from '../store/store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import { Router } from './router';
-import { store } from './store';
+import { store } from './store/store';
 
 ReactDOM.render(
   <Provider store={store}>
