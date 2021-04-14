7dff59b24867f1d9fca8c90c4b2e70a41e8b60e8
Alvin <alvinweibin@gmail.com>
2021-04-14 22:42:22 +0800

    created mock page

--- src/pages/file-url.tsx
+++ src/pages/file-url.tsx
@@ -6,12 +6,12 @@ export function FileURL() {
 
     return (
         <MainLayout>
-            <div>File URL</div>
-            <div style={{ display:'flex', height:'150px', justifyContent:'center', marginBottom:'50px' }} >
+            <div style={{ display:'flex', height:'150px', justifyContent:'center', marginBottom:'50px', marginTop:'100px' }} >
                 <img src={icaLogo} alt="ica"/>
             </div>
-            <div style={{ display:'flex', height:'40px', justifyContent:'center', alignItems:'center' }}>Mobile Number <input type="text" name="Enter Mobile Number" style={{ marginLeft:'40px', boxSizing: 'border-box', margin: 0, border: 'solid 1px' }}/></div>
-            <div style={{ display:'flex', height:'40px', justifyContent:'center', alignItems:'center' }}>One-Time Pin <input type="text" name="Enter Mobile Number" style={{ marginLeft:'40px', boxSizing: 'border-box', margin: 0, border: 'solid 1px' }}/></div>
+            <div style={{ display:'flex', height:'40px', justifyContent:'center', alignItems:'center' }}>Mobile Number <input type="text" name="Enter Mobile Number" style={{ marginLeft:'50px', boxSizing: 'border-box', border: 'solid 1px', borderRadius: '4px' }}/></div>
+            <div style={{ display:'flex', height:'40px', justifyContent:'center', alignItems:'center' }}>One-Time Pin <input type="text" name="Enter Mobile Number" style={{ marginLeft:'50px', boxSizing: 'border-box', border: 'solid 1px', borderRadius: '4px' }}/></div>
+            <div style={{ display:'flex', justifyContent:'center', marginTop:'30px', marginBottom:'200px' }} ><button type="button" style={{ width:'wrap-content',paddingLeft:'20px', paddingRight:'20px', border: 'solid 1px', borderRadius: '5px' }}> Retrieve File </button></div>
         </MainLayout>
     )
 }
